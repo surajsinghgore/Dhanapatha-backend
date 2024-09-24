1c0ac5a3768f19d7b73d8df27c76fa5e42b0e38f
suraj singh rajput <surajthakurrs45@gmail.com>
2024-09-24 09:14:55 +0530

    changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { DbConnection } from "./db/DbConnection.js";
+import {DbConnection} from "./db/DbConnection.js";
 import { app } from "./app.js"; 
 
 const server = express(); 
@@ -13,16 +13,16 @@ const corsOptions = {
 };
 
 server.use(cors(corsOptions));
+
 server.use(app); 
 
-// Connect to the database
 DbConnection()
   .then(() => {
-    console.log("⚙️ MongoDB connection established.");
+    const PORT = process.env.PORT || 8000;
+    server.listen(PORT, () => {
+      console.log(`⚙️ Server is running at port : ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.error("MongoDB connection failed:", err);
+    console.log("MONGO db connection failed !!! ", err);
   });
-
-// Export the server as a serverless function
-export default server;
