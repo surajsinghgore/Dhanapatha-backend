57b49a9072c91467bc06233f4940667301a39965
suraj singh rajput <surajthakurrs45@gmail.com>
2024-10-01 08:28:59 +0530

    api modified

--- controllers/Account.controller.js
+++ controllers/Account.controller.js
@@ -1,9 +1,7 @@
 import { User } from "../models/User.model.js";
-import {Transaction} from "../models/Transaction.model.js"
-import AddMoney from "../models/AddMoney.model.js"
+import { Transaction } from "../models/Transaction.model.js";
+import AddMoney from "../models/AddMoney.model.js";
 import { formatDateTime } from "../handlers/handler.js";
-
-
 
 export const addAccount = async (req, res) => {
   try {
@@ -13,41 +11,37 @@ export const addAccount = async (req, res) => {
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ status:false, message: "User not found" });
+      return res.status(404).json({ status: false, message: "User not found" });
     }
 
-    if (
-      user.bankAccountDetails &&
-      user.bankAccountDetails.accountNumber === accountNumber &&
-      user.bankAccountDetails.ifscCode === ifscCode
-    ) {
-      return res.status(400).json({  status:false,message: "You have already added this account." });
+    if (user.bankAccountDetails && user.bankAccountDetails.accountNumber === accountNumber && user.bankAccountDetails.ifscCode === ifscCode) {
+      return res.status(400).json({ status: false, message: "You have already added this account." });
     }
 
-    const existingUser = await User.findOne({ 
-      "bankAccountDetails.accountNumber": accountNumber, 
-      "bankAccountDetails.ifscCode": ifscCode 
+    const existingUser = await User.findOne({
+      "bankAccountDetails.accountNumber": accountNumber,
+      "bankAccountDetails.ifscCode": ifscCode,
     });
 
     if (existingUser) {
-      return res.status(400).json({  status:false,message: "This combination of account number and IFSC code already exists for another user." });
+      return res.status(400).json({ status: false, message: "This combination of account number and IFSC code already exists for another user." });
     }
 
     if (user.bankAccountDetails && user.bankAccountDetails.accountNumber) {
-      return res.status(400).json({ status:false, message: "Bank account already exists. You cannot add another." });
+      return res.status(400).json({ status: false, message: "Bank account already exists. You cannot add another." });
     }
 
     const accountNumberRegex = /^\d{9,18}$/;
-    const ifscCodeRegex = /^[A-Z]{4}0[A-Z0-9]{6}$/; 
+    const ifscCodeRegex = /^[A-Z]{4}0[A-Z0-9]{6}$/;
 
     if (!accountNumberRegex.test(accountNumber)) {
-      return res.status(400).json({  status:false,message: "Invalid account number. Must be between 9 and 18 digits." });
+      return res.status(400).json({ status: false, message: "Invalid account number. Must be between 9 and 18 digits." });
     }
-    if (!ifscCodeRegex.test(ifscCode)) { 
-      return res.status(400).json({  status:false,message: "Invalid IFSC code. Must follow the format 'ABCD0000123'." });
+    if (!ifscCodeRegex.test(ifscCode)) {
+      return res.status(400).json({ status: false, message: "Invalid IFSC code. Must follow the format 'ABCD0000123'." });
     }
     if (!["checking", "savings", "business"].includes(accountType)) {
-      return res.status(400).json({  status:false,message: "Invalid account type. Must be 'checking', 'savings', or 'business'." });
+      return res.status(400).json({ status: false, message: "Invalid account type. Must be 'checking', 'savings', or 'business'." });
     }
 
     // Save bank account details
@@ -56,19 +50,17 @@ export const addAccount = async (req, res) => {
       accountNumber,
       ifscCode,
       bankName,
-      accountType
+      accountType,
     };
 
     await user.save();
 
-    return res.status(200).json({  status:true,message: "Bank details added successfully", user });
-
+    return res.status(200).json({ status: true, message: "Bank details added successfully", user });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({  status:false,message: "Internal server error" });
+    return res.status(500).json({ status: false, message: "Internal server error" });
   }
 };
-
 
 export const updateAccount = async (req, res) => {
   try {
@@ -101,19 +93,16 @@ export const updateAccount = async (req, res) => {
     }
 
     // Check if the account number and IFSC code are being updated, only then check for duplicates
-    if (
-      (accountNumber && accountNumber !== user.bankAccountDetails.accountNumber) ||
-      (ifscCode && ifscCode !== user.bankAccountDetails.ifscCode)
-    ) {
+    if ((accountNumber && accountNumber !== user.bankAccountDetails.accountNumber) || (ifscCode && ifscCode !== user.bankAccountDetails.ifscCode)) {
       const existingUser = await User.findOne({
         "bankAccountDetails.accountNumber": accountNumber,
-        "bankAccountDetails.ifscCode": ifscCode
+        "bankAccountDetails.ifscCode": ifscCode,
       });
 
       if (existingUser && existingUser._id.toString() !== userId) {
         return res.status(400).json({
           status: false,
-          message: "This combination of account number and IFSC code already exists for another user."
+          message: "This combination of account number and IFSC code already exists for another user.",
         });
       }
     }
@@ -129,61 +118,56 @@ export const updateAccount = async (req, res) => {
     await user.save();
 
     return res.status(200).json({ status: true, message: "Bank details updated successfully", user });
-
   } catch (error) {
     console.error(error);
     return res.status(500).json({ status: false, message: "Internal server error" });
   }
 };
 
-
 export const fetchAccountBalance = async (req, res) => {
   try {
     const { _id } = req.user;
 
-const balance=await User.findById(_id).select("balance");
+    const balance = await User.findById(_id).select("balance");
 
-if(!balance){
-  return res.status(404).json({ success: false, message: "User not found" });
-}
+    if (!balance) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
 
-return res.status(200).json({ success: true, data:balance });
-
-
+    return res.status(200).json({ success: true, data: balance });
   } catch (error) {
     console.error("Stripe balance get:", error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
 
-
 export const transferMoney = async (req, res) => {
   try {
     const { receiverEmail, amount } = req.body;
     const senderId = req.user._id;
 
-    const amountInt = parseInt(amount, 10); 
+    const amountInt = parseInt(amount, 10);
 
     if (!amountInt || amountInt <= 0) {
-      return res.status(400).json({ success: false, message: 'Amount must be a positive number' });
+      return res.status(400).json({ success: false, message: "Amount must be a positive number" });
     }
     if (!receiverEmail) {
-      return res.status(400).json({ success: false, message: 'Please provide receiver email id' });
+      return res.status(400).json({ success: false, message: "Please provide receiver email id" });
     }
 
     const sender = await User.findById(senderId);
     const receiver = await User.findOne({ email: receiverEmail });
 
     if (!receiver) {
-      return res.status(404).json({ success: false, message: 'Receiver not found' });
+      return res.status(404).json({ success: false, message: "Receiver not found" });
     }
 
     if (!sender) {
-      return res.status(404).json({ success: false, message: 'Sender not found' });
+      return res.status(404).json({ success: false, message: "Sender not found" });
     }
 
     if (sender.balance < amountInt) {
-      return res.status(400).json({ success: false, message: 'Insufficient balance' });
+      return res.status(400).json({ success: false, message: "Insufficient balance" });
     }
 
     const transaction = await Transaction.create({
@@ -195,53 +179,43 @@ export const transferMoney = async (req, res) => {
     await sender.deductBalance(amountInt); // Use the converted integer amount
     await receiver.incrementBalance(amountInt); // Use the converted integer amount
 
-    res.status(200).json({ success: true, message: 'Transfer successful', transaction, status: transaction.status });
+    res.status(200).json({ success: true, message: "Transfer successful", transaction, status: transaction.status });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
-
-
 
 export const fetchTransactions = async (req, res) => {
   try {
     const userId = req.user._id;
 
     const transactions = await Transaction.find({
-      $or: [
-        { senderId: userId },
-        { receiverId: userId }
-      ]
+      $or: [{ senderId: userId }, { receiverId: userId }],
     })
-    .sort({ createdAt: -1 })
-    .populate('senderId', 'username email') 
-    .populate('receiverId', 'username email')  
-    .exec();
-
-
-    const addMoneyTransactions = await AddMoney.find({ userId })
       .sort({ createdAt: -1 })
+      .populate("senderId", "username email")
+      .populate("receiverId", "username email")
       .exec();
 
- 
-    const formattedTransactions = transactions.map(transaction => ({
+    const addMoneyTransactions = await AddMoney.find({ userId }).sort({ createdAt: -1 }).exec();
+
+    const formattedTransactions = transactions.map((transaction) => ({
       transactionId: transaction.transactionId,
       amount: transaction.amount,
-      type: 'Transfer', 
-      senderUsername: transaction.senderId.username, 
-      receiver: transaction.receiverId, 
+      type: "Transfer",
+      senderUsername: transaction.senderId.username,
+      receiver: transaction.receiverId,
       status: transaction.status,
-      createdAt: transaction.createdAt
+      createdAt: transaction.createdAt,
     }));
 
-
-    const formattedAddMoneyTransactions = addMoneyTransactions.map(addMoney => ({
+    const formattedAddMoneyTransactions = addMoneyTransactions.map((addMoney) => ({
       transactionId: addMoney.transactionId,
       amount: addMoney.amount,
-      type: 'Add Money',
+      type: "Add Money",
       status: addMoney.status,
-      createdAt: addMoney.createdAt
+      createdAt: addMoney.createdAt,
     }));
 
     const allTransactions = [...formattedTransactions, ...formattedAddMoneyTransactions];
@@ -250,11 +224,9 @@ export const fetchTransactions = async (req, res) => {
     res.status(200).json({ success: true, transactions: allTransactions });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
-
-
 
 export const getBankDetails = async (req, res) => {
   try {
@@ -262,22 +234,19 @@ export const getBankDetails = async (req, res) => {
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ status:false,message: "User not found" });
+      return res.status(404).json({ status: false, message: "User not found" });
     }
 
     if (!user.bankAccountDetails || !user.bankAccountDetails.accountNumber) {
-      return res.status(200).json({ status:true,message: "No bank account details found." });
+      return res.status(200).json({ status: true, message: "No bank account details found." });
     }
 
     return res.status(200).json({ message: "Bank account details retrieved successfully", bankAccountDetails: user.bankAccountDetails });
-
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ status:false,message: "Internal server error" });
+    return res.status(500).json({ status: false, message: "Internal server error" });
   }
 };
-
-
 
 export const uniqueReceivers = async (req, res) => {
   try {
@@ -288,42 +257,42 @@ export const uniqueReceivers = async (req, res) => {
       {
         $match: {
           senderId: userId,
-          receiverId: { $ne: userId } // Ensure the receiver is not the same as the sender
-        }
+          receiverId: { $ne: userId }, // Ensure the receiver is not the same as the sender
+        },
       },
       {
         $group: {
-          _id: '$receiverId', // Group by receiverId
-          lastTransaction: { $max: '$createdAt' }, // Get the latest transaction date
-          amount: { $first: '$amount' }, // Get the amount (you can adjust this based on your needs)
-          status: { $first: '$status' }, // Get the status (you can adjust this based on your needs)
-        }
+          _id: "$receiverId", // Group by receiverId
+          lastTransaction: { $max: "$createdAt" }, // Get the latest transaction date
+          amount: { $first: "$amount" }, // Get the amount (you can adjust this based on your needs)
+          status: { $first: "$status" }, // Get the status (you can adjust this based on your needs)
+        },
       },
       {
         $lookup: {
-          from: 'users', // The collection name for users
-          localField: '_id',
-          foreignField: '_id',
-          as: 'receiverDetails'
-        }
+          from: "users", // The collection name for users
+          localField: "_id",
+          foreignField: "_id",
+          as: "receiverDetails",
+        },
       },
       {
-        $unwind: '$receiverDetails' 
+        $unwind: "$receiverDetails",
       },
       {
         $project: {
           _id: 0,
-          receiverId: '$_id',
-          username: '$receiverDetails.username',
-          email: '$receiverDetails.email',
+          receiverId: "$_id",
+          username: "$receiverDetails.username",
+          email: "$receiverDetails.email",
           lastTransaction: 1,
           amount: 1,
-          status: 1
-        }
+          status: 1,
+        },
       },
       {
-        $sort: { lastTransaction: -1 }
-      }
+        $sort: { lastTransaction: -1 },
+      },
     ]);
 
     return res.status(200).json({ receivers: transactions });
@@ -333,30 +302,23 @@ export const uniqueReceivers = async (req, res) => {
   }
 };
 
-
-
-
 export const getTransactionsByReceiverEmail = async (req, res) => {
   try {
     const { email } = req.query;
 
-       const receiver = await User.findOne({ email });
+    const receiver = await User.findOne({ email });
 
     if (!receiver) {
       return res.status(404).json({ message: "Receiver not found." });
     }
 
     const transactions = await Transaction.find({
-      $or: [
-        { receiverId: receiver._id },  
-        { senderId: receiver._id }      
-      ]
-    }).sort({ createdAt: 1 }); 
-
+      $or: [{ receiverId: receiver._id }, { senderId: receiver._id }],
+    }).sort({ createdAt: 1 });
 
     const groupedTransactions = transactions.reduce((acc, transaction) => {
-      const formattedDate = formatDateTime(transaction.createdAt).split(",")[0]; 
-      const formattedTime = formatDateTime(transaction.createdAt).split(",")[1].trim(); 
+      const formattedDate = formatDateTime(transaction.createdAt).split(",")[0];
+      const formattedTime = formatDateTime(transaction.createdAt).split(",")[1].trim();
       if (!acc[formattedDate]) {
         acc[formattedDate] = [];
       }
@@ -384,21 +346,16 @@ export const getTransactionsByReceiverEmail = async (req, res) => {
   }
 };
 
-
 export const getSenderData = async (req, res) => {
   try {
-    const userId = req.user._id;  
+    const userId = req.user._id;
 
-    const transactions = await Transaction.find({ senderId: userId })
-      .sort({ createdAt: -1 }) 
-      .populate('receiverId', 'username email') 
-      .exec();
-
+    const transactions = await Transaction.find({ senderId: userId }).sort({ createdAt: -1 }).populate("receiverId", "username email").exec();
 
     const formatDate = (date) => {
       const d = new Date(date);
-      const day = String(d.getDate()).padStart(2, '0');  
-      const month = String(d.getMonth() + 1).padStart(2, '0'); 
+      const day = String(d.getDate()).padStart(2, "0");
+      const month = String(d.getMonth() + 1).padStart(2, "0");
       const year = d.getFullYear();
       return `${day}/${month}/${year}`;
     };
@@ -406,10 +363,8 @@ export const getSenderData = async (req, res) => {
     const groupedTransactions = transactions.reduce((acc, tx) => {
       const formattedDate = formatDate(tx.createdAt);
 
-
-      let dateGroup = acc.find(group => group.date === formattedDate);
+      let dateGroup = acc.find((group) => group.date === formattedDate);
       if (!dateGroup) {
-  
         dateGroup = { date: formattedDate, allData: [] };
         acc.push(dateGroup);
       }
@@ -417,9 +372,9 @@ export const getSenderData = async (req, res) => {
       dateGroup.allData.push({
         _id: tx._id,
         amount: tx.amount,
-        user: tx.receiverId, 
-        status: tx.status,
-        transactionId: tx.transactionId
+        user: tx.receiverId,
+        status: "sender",
+        transactionId: tx.transactionId,
       });
 
       return acc;
@@ -427,7 +382,7 @@ export const getSenderData = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      transactions: groupedTransactions
+      transactions: groupedTransactions,
     });
   } catch (error) {
     console.error(error);
@@ -435,21 +390,16 @@ export const getSenderData = async (req, res) => {
   }
 };
 
-
-
 export const getReceiverData = async (req, res) => {
   try {
-    const userId = req.user._id; 
+    const userId = req.user._id;
 
-    const transactions = await Transaction.find({ receiverId: userId })
-      .sort({ createdAt: -1 }) 
-      .populate('senderId', 'username email')  
-      .exec();
+    const transactions = await Transaction.find({ receiverId: userId }).sort({ createdAt: -1 }).populate("senderId", "username email").exec();
 
     const formatDate = (date) => {
       const d = new Date(date);
-      const day = String(d.getDate()).padStart(2, '0');  
-      const month = String(d.getMonth() + 1).padStart(2, '0'); 
+      const day = String(d.getDate()).padStart(2, "0");
+      const month = String(d.getMonth() + 1).padStart(2, "0");
       const year = d.getFullYear();
       return `${day}/${month}/${year}`;
     };
@@ -457,7 +407,7 @@ export const getReceiverData = async (req, res) => {
     const groupedTransactions = transactions.reduce((acc, tx) => {
       const formattedDate = formatDate(tx.createdAt);
 
-      let dateGroup = acc.find(group => group.date === formattedDate);
+      let dateGroup = acc.find((group) => group.date === formattedDate);
       if (!dateGroup) {
         dateGroup = { date: formattedDate, allData: [] };
         acc.push(dateGroup);
@@ -466,9 +416,10 @@ export const getReceiverData = async (req, res) => {
       dateGroup.allData.push({
         _id: tx._id,
         amount: tx.amount,
-        user: tx.senderId, 
-        status: tx.status,
-        transactionId: tx.transactionId
+        user: tx.senderId,
+        status: "receiver",
+
+        transactionId: tx.transactionId,
       });
 
       return acc;
@@ -476,7 +427,7 @@ export const getReceiverData = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      transactions: groupedTransactions
+      transactions: groupedTransactions,
     });
   } catch (error) {
     console.error(error);
